fb0ad254d6d5306803d9685e1cda53874ca0077a
Altan Emre <altan_emre_1989@hotmail.com>
2024-08-17 15:41:53 +0300

    Array Continue

---
+++ 08_Try_Catch/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _08_Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TRY_CATCH: Fırlatılan Exception'ları yakalarak program akışını durdurmadan hata durumunda ne yapılması gerektiğini tanımlar.
+
+
+
+
+            //try
+            //{
+            //    Console.WriteLine("Bir sayı giriniz");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine(sayi / 0);
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Sayı rakamlardan oluşur!!!");
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("Sayı 0 a bölünemez!!!");
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Bilinmeyen Hata!!!");
+            //}
+
+
+            //try
+            //{
+            //    Console.WriteLine("Bir sayı giriniz");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine(sayi / 0);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //    Console.WriteLine(e.Message);
+            //}
+
+
+            #region Kullanıcıdan iki sayı alınacak ve toplamı yazdırılacak.
+            // iki sayıdan biri hatalı girilirse o sayı tekrar istenilecek.
+            #endregion
+        }
+    }
+}
---
+++ 10_Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _10_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 *******  ARRAYS - DİZİLER *******
+                 *Değişkenlerin aksine diziler Index adı verilen numaralandırma yöntemi ile birden fazla değeri tutabilen koleksiyon türüdür.
+                 *Index numaraları 0 dan başlar ve 1'er 1'er artar.
+                 *Arrays kendileri aitm metotlara sahiptirler.
+                 *Array [] ile tanımlanır.
+                 *Tek veri tipinde verilen uzunluk kadar elaman tutar.
+
+             //DİZİ TANIMLAMA
+
+            //veriTipi[] diziAdi = new veriTipi[uzunluk]
+
+            //Boş Dizi Tanımı
+            string[] isimler = new string[5];
+
+            //Dolu Dizi Tanımı
+            string[] adlar = { "Mehmet", "Ercan", "Bahir", "Arhan", "Koray" };
+
+             */
+
+
+
+
+
+
+        }
+    }
+}
